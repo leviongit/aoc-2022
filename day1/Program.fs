9c9a8438659efd280772e71414503ecbc83482e7
leviongit <levi.duncan.self@gmail.com>
2022-12-02 15:53:44 +0100

    make the program slightly better

--- day1/Program.fs
+++ day1/Program.fs
@@ -1,26 +1,17 @@
 ﻿open System.IO
 
-let SplitOn (elem: 'a) (ary: 'a []) : 'a [] [] =
-  let rec SplitOn (acc: 'a [] []) (curr: 'a list) (list: 'a list) (elem: 'a) =
-    match list with
-    | e :: tail when e.Equals(elem) -> SplitOn (Array.append acc [| (Array.ofList (List.rev curr)) |]) [] tail elem
-    | e :: tail -> SplitOn acc (e :: curr) tail elem
-    | [] -> acc
+let SplitOnDoubleNewlineAndConvertToInt (string: string) : int[][] =
+  string.Split "\n\n"
+  |> Array.map (fun s -> Array.map int (s.Split '\n'))
 
-  SplitOn [||] [] (List.ofArray ary) elem
 
-let SplitOnDoubleNewline (string: string) : string [] [] = string.Split '\n' |> SplitOn ""
 
-let ToNumbers (ary: string [] []) : int [] [] = Array.map (Array.map (int)) ary
-
-let LoadFile (filename: string) : string = File.ReadAllText filename
+let inline LoadFile (filename: string) : string = File.ReadAllText filename
 
 let values =
   LoadFile "in.txt"
-  |> SplitOnDoubleNewline
-  |> ToNumbers
+  |> SplitOnDoubleNewlineAndConvertToInt
   |> Array.map (Array.sum)
-  // |> Array.max
   
 let singularValue = Array.max values
   
